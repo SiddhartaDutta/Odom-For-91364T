65bcbd5b3d593b1187e42076c18ef54f96b68bc2
SiddhartaDutta <52301934+SiddhartaDutta@users.noreply.github.com>
2021-04-08 00:24:34 -0700

    Created odometry.h
    Added file and a simple header.

---
+++ include/odometry.h
@@ -0,0 +1,8 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       odometry.h                                                */
+/*    Author:       Siddharta Dutta                                           */
+/*    Created:      Thu Apr 08 2021                                           */
+/*    Description:  Contains all odometry algorithm declarations              */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
