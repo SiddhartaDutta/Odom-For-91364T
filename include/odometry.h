fbb07feda3647c3630cd17ea9f894b4d09961380
SiddhartaDutta <52301934+SiddhartaDutta@users.noreply.github.com>
2021-04-08 01:12:20 -0700

    Update odometry.h
    Added namespace + declarations

--- include/odometry.h
+++ include/odometry.h
@@ -6,3 +6,34 @@
 /*    Description:  Contains all odometry algorithm declarations              */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
+
+#ifndef ODOMETRY_H
+#define ODOMETRY_H
+
+namespace OdomLib {
+  
+    // Enables/Disables odometry task
+  extern bool enable_odom;
+  
+    // Function for odometry task
+  int updatePosition();
+  
+    // Functions for accessing coordinates and heading
+  float getXPos();
+  float getYPos();
+  float getHeading();
+  float getHeadingDeg();
+  
+    // Clears all odometry relavant values to default (0) and sets inputted values (if provided)
+  void setOdometry(float setX = 0, float setY = 0, float setHeading = 0);
+  
+    // Clears all odometry encoders
+  void clearEncoders();
+  
+    // Turns to given heading
+  void pointTurn(float deg);
+  
+    // MAJOR ISSUE: WORKS EXCLUSIVELY IN Y-DIREC 
+  void driveTo(float target);
+  
+}
